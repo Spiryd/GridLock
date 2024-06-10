84d108d8e034271bd1477f6b13bcd74cac7d089c
Spiryd <neumann.maks@gmail.com>
2024-06-10 23:25:57 +0200

    changed serialization to pickle

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use grid_lock::{GridLock, N, P};
 
 use dialoguer::{theme::ColorfulTheme, Select, Input};
 
+use serde_pickle::{SerOptions, DeOptions};
+
 fn main() {
     let mut grid_lock = GridLock::new();
     let selections = &["Generate Key Pair", "Encrypt", "Decrypt", "Exit"];
@@ -23,46 +25,63 @@ fn main() {
             0 => {
                 let secret_key = grid_lock.gen_secret_key();
                 println!("Secret Key Generated");
-                let encoded_secret_key = ron::to_string(&secret_key).unwrap();
                 println!("Saving Secret Key");
-                std::fs::write("sk.ron", encoded_secret_key).expect("Failed to write secret key to file");
-                println!("Secret Key saved to sk.ron");
+                let mut secret_key_file = std::fs::File::create("sk").expect("Failed to create secret key file");
+                serde_pickle::to_writer(
+                    &mut secret_key_file,
+                    &secret_key,
+                    SerOptions::default(),
+                ).expect("Failed to write secret key to file");
+                println!("Secret Key saved to sk");
                 let public_key = grid_lock.gen_public_key(&secret_key);
                 println!("Public Key Generated");
-                let encoded_public_key = ron::to_string(&public_key).unwrap();
                 println!("Saving Public Key");
-                std::fs::write("pk.ron", encoded_public_key).expect("Failed to write public key to file");
-                println!("Public Key saved to pk.ron");
+                let mut public_key_file = std::fs::File::create("pk").expect("Failed to create public key file");
+                serde_pickle::to_writer(
+                    &mut public_key_file,
+                    &public_key,
+                    SerOptions::default(),
+                ).expect("Failed to write public key to file");
+                println!("Public Key saved to pk");
             }
             1 => {
-                let encoded_public_key = std::fs::read_to_string("pk.ron").expect("Failed to read public key from file");
-                let public_key: Vec<(Vec<z::Z>, z::Z)> = ron::from_str(&encoded_public_key).unwrap();
+                let mut pk_file = std::fs::File::open("pk").expect("Failed to open public key file");
+                let public_key: Vec<(Vec<z::Z>, z::Z)> = serde_pickle::from_reader(
+                    &mut pk_file,
+                    DeOptions::default(),
+                ).expect("Failed to read public key from file");
                 println!("Public Key Loaded");
                 let encryptee_path: String = Input::with_theme(&ColorfulTheme::default())
                     .with_prompt("Path to file to encrypt")
                     .interact_text()
                     .unwrap();
-                let encryptee = BitVec::from_bytes(std::fs::read(encryptee_path).unwrap());
+                let encryptee = BitVec::from_bytes(std::fs::read(&encryptee_path).unwrap());
                 let encrypted = grid_lock.encrypt(&public_key, encryptee);
                 println!("Encryption Complete");
-                let encoded_encrypted = ron::to_string(&encrypted).unwrap();
-                let encrypted_path: String = Input::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Path to save encrypted file")
-                    .interact_text()
-                    .unwrap();
-                std::fs::write(&encrypted_path, encoded_encrypted).expect("Failed to write encrypted file to disk");
-                println!("Encrypted file saved to {}", encrypted_path);
+                let mut encripted_file = std::fs::File::create(format!("{}.gl", &encryptee_path)).expect("Failed to create encrypted file");
+                serde_pickle::to_writer(
+                    &mut encripted_file,
+                    &encrypted,
+                    SerOptions::default(),
+                ).expect("Failed to write encrypted file to disk");
+                println!("Encrypted file saved to {}.gl", &encryptee_path);
             }
             2 => {
-                let encoded_secret_key = std::fs::read_to_string("sk.ron").expect("Failed to read secret key from file");
-                let secret_key: Vec<z::Z> = ron::from_str(&encoded_secret_key).unwrap();
+                let mut sk_file = std::fs::File::open("sk").expect("Failed to open secret key file");
+                let secret_key: Vec<z::Z> = serde_pickle::from_reader(
+                    &mut sk_file,
+                    DeOptions::default(),
+                ).expect("Failed to read secret key from file");
                 println!("Secret Key Loaded");
                 let encrypted_path: String = Input::with_theme(&ColorfulTheme::default())
                     .with_prompt("Path to file to decrypt")
                     .interact_text()
                     .unwrap();
-                let encoded_encrypted = std::fs::read_to_string(encrypted_path).expect("Failed to read encrypted file from disk");
-                let encrypted: Vec<(Vec<z::Z>, z::Z)> = ron::from_str(&encoded_encrypted).unwrap();
+                let mut encrypted_file = std::fs::File::open(&encrypted_path).expect("Failed to open encrypted file");
+                let encrypted: Vec<(Vec<z::Z>, z::Z)> = serde_pickle::from_reader(
+                    &mut encrypted_file,
+                    DeOptions::default(),
+                ).expect("Failed to read encrypted file from disk");
                 let decrypted = grid_lock.decrypt(&secret_key, &encrypted);
                 println!("Decryption Complete");
                 let decrypted_path: String = Input::with_theme(&ColorfulTheme::default())
